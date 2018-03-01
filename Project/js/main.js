40ced77fded689f3c135d613a7a5d1d3aacf1d0f
alboin <albinbergstrom@gmail.com>
2018-03-01 16:00:42 +0100

    Scatterplot added

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,23 +1,44 @@
-    var width = 600,                        
-    height = 600,                            
+// Load data
+queue()
+  .defer(d3.csv,'data/categories.csv')
+  .defer(d3.csv,'data/months.csv')
+  .await(draw);
+
+$("body").height($(document).height());
+$("body").width($(document).width());
+
+var scatterplot;
+
+function draw(error, data_c, data_m){
+  if (error) throw error;
+
+  scatterplot = new scatterplot(data_c);
+
+}
+    var div = '#pie-chart';
+
+    console.log($(div).parent().width())
+
+    var width = 500,//$(div).parent().width(),
+    height = 400,//$(div).parent().width(),
     maxRadius = 200,
-    minRadius = 100,    
+    minRadius = 100,
     color = d3.scale.category20c();     //builtin range of colors
 
-    data = [{"Month":"Jan", "success":7122, "failed":17141}, 
-            {"Month":"Feb", "success":8358, "failed":16127}, 
+    data = [{"Month":"Jan", "success":7122, "failed":17141},
+            {"Month":"Feb", "success":8358, "failed":16127},
             {"Month":"Mar", "success":12137, "failed":20437},
-            {"Month":"Apr", "success":12344, "failed":20222}, 
-            {"Month":"May", "success":12604, "failed":21155}, 
+            {"Month":"Apr", "success":12344, "failed":20222},
+            {"Month":"May", "success":12604, "failed":21155},
             {"Month":"Jun", "success":11711, "failed":20339},
-            {"Month":"Jul", "success":12103, "failed":21600}, 
-            {"Month":"Aug", "success":11959, "failed":24330}, 
+            {"Month":"Jul", "success":12103, "failed":21600},
+            {"Month":"Aug", "success":11959, "failed":24330},
             {"Month":"Sep", "success":10551, "failed":21209},
-            {"Month":"Oct", "success":11467, "failed":20565}, 
-            {"Month":"Nov", "success":11801, "failed":20189}, 
+            {"Month":"Oct", "success":11467, "failed":20565},
+            {"Month":"Nov", "success":11801, "failed":20189},
             {"Month":"Dec", "success":11799, "failed":11799}];
-    
-    var vis = d3.select("body")
+
+    var vis = d3.select(div)
         .append("svg:svg")              //create the SVG element inside the <body>
         .data([data])                   //associate our data with the document
             .attr("width", width)           //set the width and height of our visualization (these will be attributes of the <svg> tag
@@ -27,11 +48,11 @@
 
     var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
         //.outerRadius(maxRadius)
-        .outerRadius(function(d) { 
-        	
+        .outerRadius(function(d) {
+
         	var radius = minRadius + d.data.success/(d.data.failed+d.data.success) * 100;
 
-        	return radius ; 
+        	return radius ;
         })
         .padRadius(30);
 
@@ -40,7 +61,7 @@
         .value(function(d) { return 100; });    //return the val controlling the arc angle
 
     var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
-        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
+        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
         .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
             .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
                 .attr("class", "slice");    //allow us to style things in the slices (like text)
@@ -60,6 +81,3 @@
             })
             .attr("text-anchor", "middle")                          //center the text on it's origin
             .text(function(d, i) { return data[i].Month; });        //get the label from our original data array
-		
-
-	
---
+++ Project/js/scatterplot.js
@@ -0,0 +1,159 @@
+function scatterplot(data)
+{
+  // Format data so that numbers are numbers and not strings.
+  for(sample in data)
+    for(point in data[sample])
+      if(!isNaN(Number(data[sample][point])))
+        data[sample][point] = Number(data[sample][point]);
+
+  console.log(data);
+
+  var div = '#scatter-plot';
+
+  //The base-code for the scatterplot is from
+  //http://bl.ocks.org/weiglemc/6185069
+
+
+  var width = $(div).parent().width();
+  var height = 500;
+  var margin = {top: 50, right: 50, bottom: 60, left: 70};
+  console.log(height)
+
+  // setup fill color
+  var cValue = function(d) { return d.maincategory;},
+    color = d3.scale.category10();
+
+    // add the tooltip area to the webpage
+  var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("width", 100)
+    .style("height", 18)
+    .style("opacity", 0);
+
+
+  // setup x
+  var xValue = function(d) { return Number(d.backers)}, // data -> value
+      xScale = d3.scale.log().range([0, width]), // value -> display
+      xMap = function(d) { return xScale(xValue(d));}, // data -> display
+      xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+  // setup y
+  var yValue = function(d) { return Number(d.goal);}, // data -> value
+      yScale = d3.scale.log().range([height, 0]), // value -> display
+      yMap = function(d) { return yScale(yValue(d));}, // data -> display
+      yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+  // add the graph canvas to the body of the webpage
+  var svg = d3.select(div).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // don't want dots overlapping axis, so add in buffer to data domain
+    xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
+    yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+    // x-axis
+ svg.append("g")
+     .attr("class", "x axis")
+     .attr("transform", "translate(0," + height + ")")
+     .call(xAxis.ticks(4))
+   .append("text")
+     .attr("class", "label")
+     .attr("x", width)
+     .attr("y", -6)
+     .style("text-anchor", "end")
+     .text("Backers");
+
+ // y-axis
+ svg.append("g")
+     .attr("class", "y axis")
+     .call(yAxis)
+   .append("text")
+     .attr("class", "label")
+     .attr("transform", "rotate(-90)")
+     .attr("y", 6)
+     .attr("dy", ".71em")
+     .style("text-anchor", "end")
+     .text("Goal sum");
+
+ // draw dots
+ svg.selectAll(".dot")
+     .data(data)
+   .enter().append("circle")
+     .attr("class", "dot")
+     .attr("r", 4.0)
+     .attr("cx", xMap)
+     .attr("cy", yMap)
+     .style("fill", function(d) { return color(cValue(d));})
+     .on("mouseover", function(d) {
+         tooltip.transition()
+              .duration(50)
+              .style("opacity", .9);
+         tooltip.html(d.category/* + "<br/> (" + xValue(d)
+         + ", " + yValue(d) + ")"*/)
+              .style("left", (d3.event.pageX + 5) + "px")
+              .style("top", (d3.event.pageY - 28) + "px");
+     })
+     .on("mouseout", function(d) {
+         tooltip.transition()
+              .duration(200)
+              .style("opacity", 0);
+     });
+
+       // draw legend
+  var legend = svg.selectAll(".legend")
+      .data(color.domain())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  // draw legend colored rectangles
+  legend.append("rect")
+      .attr("x", width - 10)
+      .attr("width", 10)
+      .attr("height", 10)
+      .style("fill", color);
+
+  // draw legend text
+  legend.append("text")
+      .attr("x", width - 15)
+      .attr("y", 5)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d;})
+
+    //Function originally from
+    //https://gist.github.com/phoebebright/3098488
+    //and then modified to suit this application
+
+    $("#rescale").on("click", function(d) {
+
+
+          /*yScale.domain([0,Math.floor((Math.random()*90)+11)])  // change scale to 0, to between 10 and 100
+          vis.select(".yaxis")
+                  .transition().duration(1500).ease("sin-in-out")  // https://github.com/mbostock/d3/wiki/Transitions#wiki-d3_ease
+                  .call(yAxis);
+          vis.select(".yaxis_label")
+              .text("Rescaled Axis");
+      */
+
+      console.log("click!");
+                // setup x
+                xValue = function(d) { return Number(d.backers)}, // data -> value
+                    xScale = d3.scale.linear().range([0, width]), // value -> display
+                    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+                    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+                // setup y
+                yValue = function(d) { return Number(d.goal);}, // data -> value
+                    yScale = d3.scale.linear().range([height, 0]), // value -> display
+                    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+                    yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+
+    });
+
+
+}
